10c4f9497b1e8e149a791851e5443ec62823e553
Thanh Nguyen <thng1701@gmail.com>
2023-08-21 08:30:21 +0700

    thanh upload 21/08/2023

--- application/views/admin/nguoidung/ds_nguoidung.php
+++ application/views/admin/nguoidung/ds_nguoidung.php
@@ -22,16 +22,53 @@
 				</div>
 			</div> -->
 			<div class="row form-group">
-				<div class="col-md-6">
+				<div class="col-md-4">
 					<label>Từ ngày</label>
 					<input type="date" name="tungay" class="form-control" value="<?=$tungay?>" />
 				</div>
-				<div class="col-md-6">
+				<div class="col-md-4">
 					<label>Đến ngày</label>
 					<input type="date" name="denngay" class="form-control" value="<?=$denngay?>" />
 				</div>
-				
+
+				<!-- chọn đơn vị -->
+				<div class="col-md-4">
+					<form action="admin/ds_nguoidung" id="search_donvi" method="post">
+						<label for="lang-select">Chọn Đơn Vị</label>
+						<select name="lang" id="lang-select">
+							<option value="">--Chọn đơn vị--</option>
+							<option value="1">Văn phòng UBND Thành phố Cần Thơ</option>
+							<option value="2">Sở Thông tin và Truyền thông</option>
+							<option value="3">Sở Công Thương</option>
+							<option value="4">Sở Lao động - Thương binh và Xã hội</option>
+							<option value="5">Sở Tài chính</option>
+							<option value="6">Sở Khoa học và Công nghệ</option>
+							<option value="7">Sở Tư pháp</option>
+							<option value="8">Sở Tài nguyên và Môi trường</option>
+							<option value="9">Sở Nông nghiệp và Phát triển nông thôn</option>
+							<option value="10">Sở Kế hoạch và Đầu tư</option>
+							<option value="12">Sở Văn hóa, Thể thao và Du lịch</option>
+							<option value="13">Sở Giao thông vận tải</option>
+							<option value="14">Sở Giáo dục và Đào tạo</option>
+							<option value="15">Thành đoàn Cần Thơ</option>
+							<option value="16">Sở Y tế</option>
+							<option value="18">Ban Quản lý khu chế xuất và khu công nghiệp</option>
+							<option value="19">Sở Ngoại vụ</option>
+							<option value="20">Văn phòng Hội đồng nhân dân thành phố Cần Thơ</option>
+							<option value="21">Sở Nội vụ</option>
+							<option value="22">Sở Xây dựng</option>
+							<option value="23">Thanh tra thành phố</option>
+							<option value="25">Ban Dân tộc thành phố</option>
+							<option value="26">UBND quận Ninh Kiều</option>
+							<option value="27">UBND quận Ô Môn</option>
+							<option value="28">UBND quận Bình Thủy</option>
+						</select>
+					</form>
+				</div>
 			</div>
+			<!-- chọn đơn vị -->
+
+
 			<div class="row form-group">
 				<div class="col-md-12">
 					<input type="checkbox" name="traloi_dung" <?= ($search_traloi_dung == 1)?'checked':'' ?>> &nbsp; <label for="">Trả lời đúng</label>
@@ -64,7 +101,7 @@
 				<td>Action</td>
 			</thead>
 			<tbody>
-				<?php if (count($ds_nguoidung)>0): ?>
+				<?php if ( is_countable($ds_nguoidung) && count($ds_nguoidung) > 0): ?>
 					<?php $stt=1; foreach($ds_nguoidung as $nguoidung): ?>
 						<tr data-id="<?= $nguoidung->get('ND_ID') ?>">
 							<td class="text-right"> <?= $stt++; ?> </td>
