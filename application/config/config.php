e6affe2d87da1b9c2d79819a8faf146a20cdf1dc
NguyenDuy <duy97ct@gmail.com>
2023-08-22 08:06:15 +0700

    Duy upload trang tra cuu lan 2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/source_tracnghiem/';
+$config['base_url'] = 'http://10.172.45.102/source_tracnghiem/';
 $config['dafault_pass'] = 'long@123';
 
 /*
--- application/views/ds_ketqua.php
+++ application/views/ds_ketqua.php
@@ -5,7 +5,7 @@
 <div class="row ">
 	<div class="col-md-12 text-center mb-3">
 	<!-- <div class="col-md-3"></div> -->
-		<form action="ds_ketqua" method="POST">
+		<form action="ds_ketqua" method="POST" style="width: 800px; margin: auto">
 		<!-- <div class="row" style="margin-top:0px"> -->
 			<div class="col-12">
 			<br>
@@ -34,24 +34,29 @@
 	</div>
 </div>
 <div class="row">
-	<?php if(is_array($ds_nguoidung) && count($ds_nguoidung)>0): ?>
-		<?php foreach ($ds_nguoidung as $nguoidung) :?>
-			<div class="col-md-3 ">
+		<?php if(is_array($ds_nguoidung) && count($ds_nguoidung)>0): ?>
+			<div class="col-md-12 ">
 				<div class="div_nguoidung">
-				<table style="width: 800px; border: 1px solid black; border-collapse: collapse; text-align: center;">
-					<tr  style="background-color:skyblue">
-						<th style="border:1px solid black;">Họ tên</th>
-						<th style="border:1px solid black;">Số điện thoại</th>
-						<th style="border:1px solid black;">Địa chỉ</th>
-						<th style="border:1px solid black;">Điểm</th>
+				<table style="width: 1000px; border: 1px solid black; border-collapse: collapse; text-align: center; margin: auto; margin-bottom: 50px">
+					<tr  style="background-color:skyblue; font-size: 18px">
+						<th style="border:1px solid black; padding:5px;">STT</th>
+						<th style="border:1px solid black; padding:5px">Họ tên</th>
+						<th style="border:1px solid black; padding:5px; width: 150px">Số điện thoại</th>
+						<th style="border:1px solid black; padding:5px">Địa chỉ</th>
+						<th style="border:1px solid black; padding:5px">Đơn vị</th>
+						<th style="border:1px solid black; padding:5px">Điểm</th>
 					</tr>
+						
+							<?php foreach ($ds_nguoidung as $key => $nguoidung) :?>
 					<tr >
-						<td style="border:1px solid black; "><strong><a href="xem_kiemtra/<?= $nguoidung->get('ND_ID'); ?>"><?= $nguoidung->get('ND_TEN'); ?></a></strong><br/></td>
-						<td style="order:1px solid black;"><?= $nguoidung->get('ND_SDT'); ?></td>
-						<td style="border:1px solid black;"><?= $nguoidung->get('ND_DIA_CHI'); ?></td>
-						<td style="border:1px solid black;"><?= $nguoidung->get('ND_SO_CAU_DUNG'); ?> / <?= QUESTION_NUMBER ?></td>
+						<td style="border:1px solid black; white-space:pre-line; padding: 5px "><?= $key +1; ?> </td>
+						<td style="border:1px solid black; white-space:pre-line; padding: 5px "><strong><a href="xem_kiemtra/<?= $nguoidung->get('ND_ID'); ?>"><?= $nguoidung->get('ND_TEN'); ?></a></strong><br/></td>
+						<td style="border:1px solid black; white-space:pre-line; padding: 5px"><?= $nguoidung->get('ND_SDT'); ?></td>
+						<td style="border:1px solid black; white-space:pre-line; padding: 5px"><?= $nguoidung->get('ND_DIA_CHI'); ?></td>
+						<td style="border:1px solid black; white-space:pre-line; padding: 5px"><?= $nguoidung->get_donvi(); ?></td>
+						<td style="border:1px solid black; white-space:pre-line; padding: 5px; width: 80px"><?= $nguoidung->get('ND_SO_CAU_DUNG'); ?> / <?= QUESTION_NUMBER ?></td>
 					</tr>
-					
+					<?php endforeach;?>
 					</table>
 					<!-- <strong><a href="xem_kiemtra/<?= $nguoidung->get('ND_ID'); ?>"><?= $nguoidung->get('ND_TEN'); ?></a></strong><br/> -->
 					<!-- SĐT: <?= $nguoidung->get('ND_SDT'); ?><br/> -->
@@ -65,7 +70,7 @@
 				
 			</div>
 			
-		<?php endforeach;?>
+		
 	<?php else: ?>
 		<div class="col-md-12">
 			Không tìm thấy kết quả phù hợp
