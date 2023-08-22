cbf6150dc0daca73ce60f9d64c748ffdb315f812
NguyenDuy <duy97ct@gmail.com>
2023-08-22 09:27:51 +0700

    Duy upload trang tra cuu lan 4.2

--- application/views/kiemtra_kq.php
+++ application/views/kiemtra_kq.php
@@ -5,11 +5,30 @@
 				Chúc mừng bạn đã hoàn thành bài thi.
 			</div>
 			<div class="form-group">
-				<h5>THÔNG TIN NGƯỜI THAM GIA</h5>
-				<p>Họ tên: <?=$nguoidung->get('ND_TEN') ?></p>
+				<h5 style="color:red">THÔNG TIN NGƯỜI THAM GIA</h5>
+				<br>
+				<!-- <p>Họ tên: <?=$nguoidung->get('ND_TEN') ?></p>
 				<p>Số điện thoại: <?=$nguoidung->get('ND_SDT'); ?></p>
-				<p>Đơn vi: <?=$nguoidung->get_donvi() ?> </p>
-				<p>Thời gian thi: <?=date('H:i:s d/m/Y', strtotime($nguoidung->get('ND_NGAY_TAO'))) ?></p>
+				<p>Đơn vị: <?=$nguoidung->get_donvi() ?> </p>
+				<p>Thời gian thi: <?=date('H:i:s d/m/Y', strtotime($nguoidung->get('ND_NGAY_TAO'))) ?></p> -->
+				<table style="border: 1px solid black; border-collapse: collapse; width: 500px">
+					<tr style="border:1px solid black; padding:5px;">
+						<th style="border:1px solid black; padding:5px; background-color:skyblue">Họ tên</th>
+						<td style="border:1px solid black; padding:5px; margin: 50px; text-align: center"><?=$nguoidung->get('ND_TEN') ?></td>	
+					</tr>
+					<tr style="border:1px solid black; padding:5px;">	
+						<th style="border:1px solid black; padding:5px; background-color:skyblue">Số điện thoại</th>
+						<td style="border:1px solid black; padding:5px; text-align: center"><?=$nguoidung->get('ND_SDT') ?></td>
+					</tr>	
+					<tr style="border:1px solid black; padding:5px;">
+						<th style="border:1px solid black; padding:5px; background-color:skyblue">Đơn vị</th>
+						<td style="border:1px solid black; padding:5px; text-align: center"><?=$nguoidung->get_donvi() ?></td>
+					</tr>
+					<tr style="border:1px solid black; padding:5px;">	
+						<th style="border:1px solid black; padding:5px; background-color:skyblue">Thời gian thi</th>
+						<td style="border:1px solid black; padding:5px; text-align: center"><?=date('H:i:s d/m/Y', strtotime($nguoidung->get('ND_NGAY_TAO'))) ?></td>
+					</tr>
+				</table>
 			</div>
 			<div class="form-group">
 				<h5>KẾT QUẢ THI</h5>
